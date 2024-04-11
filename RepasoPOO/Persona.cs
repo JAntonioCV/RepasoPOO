a2fa6ad786a894cc3bcfaf010fcbe2ac8f70e0c3
Josiel Castillo <josielcastillov@gmail.com>
2024-04-11 08:17:33 -0600

    Agregar archivos de proyecto.

---
+++ RepasoPOO/Persona.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepasoPOO
+{
+    //Definicion clase persona
+    internal class Persona
+    {
+        //Atributos
+        public string Cedula { get; set; }
+
+        private string nombre;
+        public string Nombre
+        {
+            get { return nombre; }
+
+            set { nombre = value; }
+        }
+
+        public string Apellido { get; set; }
+
+        private int edad;
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        public double Peso { get; set; }
+
+        private double estatura;
+
+        public double Estatura
+        {
+            get { return estatura; }
+            set { estatura = value; }
+        }
+
+        //Constructor Simple
+        public Persona()
+        {
+
+        }
+
+        //Constructor con parametros
+        public Persona(string cedula, 
+                       string nombre, 
+                       string apellido, 
+                       int edad, 
+                       double peso,
+                       double estatura)
+        {
+            Cedula = cedula;
+            this.nombre = nombre;
+            Apellido = apellido;
+            this.edad = edad;
+            Peso = peso;
+            this.estatura = estatura;
+        }
+
+        //Metodos
+        public void MostrarInformacion(string cedulaborrar) 
+        {
+            Console.WriteLine("Cedula {0}",Cedula);
+            Console.WriteLine("Nombre {0}",nombre);
+            Console.WriteLine("Apellido {0}",Apellido);
+            Console.WriteLine("Edad {0}",edad);
+            Console.WriteLine("Peso {0}",Peso);
+            Console.WriteLine("Estatura {0}",Estatura);
+        }
+
+        private void Eliminar()
+        {
+            MostrarInformacion("001");
+        }
+    }
+}
---
+++ RepasoPOO/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepasoPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundo");
+
+            //Persona persona = new Persona();
+
+            //persona.Cedula = "0001-10057-455";
+
+            //Persona persona2 = new Persona();
+
+            //persona2.Cedula = "151561-141255-114";
+
+            //Console.WriteLine("Persona 1: " + persona.Cedula);
+            //Console.WriteLine("Persona 2: {0} {1}",persona2.Cedula, persona2.Cedula);
+            //Console.WriteLine($"Persona 2: {persona.Cedula}");
+
+            //persona.Nombre = "sakjhsdfds";
+
+
+            Persona persona = new Persona();
+
+            persona.Cedula = "0001-10057-455";
+
+
+            Console.WriteLine("Ingrese una cedula: ");
+            string cedula = Console.ReadLine();
+
+            Console.WriteLine("Ingrese una nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.WriteLine("Ingrese una apellido: ");
+            string apellido = Console.ReadLine();
+
+            Console.WriteLine("Ingrese una edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese una peso: ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese una estatura: ");
+            double estatura = double.Parse(Console.ReadLine());
+
+            Persona persona1 = new Persona(cedula,nombre,apellido,edad,peso,estatura);
+
+            persona1.MostrarInformacion("004");
+
+            List<Persona> personas = new List<Persona>();
+
+            personas.Add(persona1);
+
+
+            Console.ReadKey();
+        }
+    }
+}
